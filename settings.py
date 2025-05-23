7c8e40564bbbfbed8084d6769949af64c5a58b59
student <student@corp.vksit.ru>
2025-05-23 10:11:59 +0300

    Graphon

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 9
         self.fleet_drop_speed = 10
         self.speedup_scale = 1.1
         self.score_scale = 1.5
@@ -22,11 +22,11 @@ class Settings:
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        self.ship_speed = 1.0
+        self.ship_speed = 10.0
         self.bullet_speed = 3.0
-        self.alien_speed = 0.2
+        self.alien_speed = 2.0
         self.fleet_direction = 1
-        self.alien_points = 50
+        self.alien_points = 100
 
     def increase_speed(self):
         self.ship_speed *= self.speedup_scale
--- ship.py
+++ ship.py
@@ -7,12 +7,12 @@ class Ship(Sprite):
         super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-        self.x = float(self.rect.centerx)  # Используйте float для плавного движения
+        self.x = float(self.rect.centerx)  
         self.moving_right = False
         self.moving_left = False
 
